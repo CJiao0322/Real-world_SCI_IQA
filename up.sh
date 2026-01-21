4714eff07ef6f9d71894ecbadf3f4c88c3fb5a12
CJiao0322 <2624607876@qq.com>
2026-01-21 14:01:16 +0800

    initial commit

--- up.sh
+++ up.sh
@@ -5,5 +5,5 @@ git add .
 git commit -m "initial commit"
 
 git branch -M main
-git remote add origin git@github.com:CJiao0322/Real-world_SCI_IQA.git
+git remote add origin https://github.com/CJiao0322/Real-world_SCI_IQA.git
 git push -u origin main
