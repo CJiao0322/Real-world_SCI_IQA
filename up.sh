391b31be813e1ffcb21a853b5a920573b5730958
CJiao0322 <2624607876@qq.com>
2026-01-21 14:08:27 +0800

    initial commit

--- up.sh
+++ up.sh
@@ -7,6 +7,9 @@ git commit -m "initial commit"
 git branch -M main
 
 # ⚠️ 关键改动在这里
-git remote set-url origin https://github.com/CJiao0322/Real-world_SCI_IQA.git
+
+
+git remote set-url origin git@github.com:CJiao0322/Real-world_SCI_IQA.git
+
 
 git push -u origin main
